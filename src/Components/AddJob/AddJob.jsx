856078667d8e12f2fc51802b6b58d91d0551dafa
Tonmoy <shahariartonmoy17@gmail.com>
2023-11-28 05:59:19 +0600

    add job functionality done

--- src/Components/AddJob/AddJob.jsx
+++ src/Components/AddJob/AddJob.jsx
@@ -1,54 +1,148 @@
-const AddProduct = () => {
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/authProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+const AddJob = () => {
+
+  const { user, signOutUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  console.log(user)
   const handleNewProduct = (e) => {
     e.preventDefault();
     const form = e.target;
-    const name = form.name.value;
-    const image = form.image.value;
-    const brandName = form.brandName.value;
-    const type = form.type.value;
-    const price = form.price.value;
-    const shortDescription = form.shortDescription.value;
-    const rating = form.rating.value;
-    const newProduct = {
-      name,
-      image,
-      brandName,
-      type,
-      price,
-      shortDescription,
-      rating,
+    const email = form.email.value;
+    const title = form.jobTitle.value;
+    const deadline = form.deadline.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const minimumPrice = form.minimumPrice.value;
+    const maximumPrice = form.maximumPrice.value;
+    const imageURL = form.imageURL.value;
+    const newJob = {
+      email,
+      title,
+      deadline,
+      description,
+      category,
+      minimumPrice,
+      maximumPrice,
+      imageURL
     };
-    console.log(newProduct);
+    console.log(newJob);
     form.reset();
-    fetch("https://brand-website-server-9lu7pgb34-mirza-shahriar-tonmoys-projects.vercel.app/products", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(newProduct),
-    })
+    fetch(
+      "http://localhost:5000/jobs",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(newJob),
+      }
+    )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        toast("Job Posted successfully");
+        setTimeout(() => {
+          navigate("/")
+        }, 2000)
+      });
+
   };
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div>
+      <ToastContainer></ToastContainer>
+      <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse w-full">
         <div className="text-center lg:text-left"></div>
         <div className="card flex-shrink-0 w-full  shadow-2xl border border-[#59CE8F] bg-base-100 py-20 px-5">
           <h1 className="text-[#59CE8F] text-center text-3xl font-bold">
-            Add New Product
+            Post a Job
           </h1>
           <form className="card-body" onSubmit={handleNewProduct}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Name</span>
+                  <span className="label-text">Email</span>
                 </label>
                 <input
                   type="text"
-                  placeholder="Name"
+                  defaultValue={user.email}
+                  placeholder=""
                   className="input input-bordered"
-                  name="name"
+                  name="email"
+                  readOnly                
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Job title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Job title"
+                  className="input input-bordered"
+                  name="jobTitle"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Deadline</span>
+                </label>
+                <input
+                  type="date"
+                  className="input input-bordered"
+                  name="deadline"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Description"
+                  className="input input-bordered"
+                  name="description"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Category</span>
+                </label>
+                <select name="category" id="cars" className="input input-bordered">
+                  <option value="Web development">Web development</option>
+                  <option value="Digital marketing">Digital marketing</option>
+                  <option value="Graphics design">Graphics design</option>
+                </select>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Minimum price</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Minimum price"
+                  className="input input-bordered"
+                  name="minimumPrice"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Maximum price</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Maximum price"
+                  className="input input-bordered"
+                  name="maximumPrice"
                   required
                 />
               </div>
@@ -60,74 +154,14 @@ const AddProduct = () => {
                   type="text"
                   placeholder="Image URL"
                   className="input input-bordered"
-                  name="image"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Brand Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Brand Name"
-                  className="input input-bordered"
-                  name="brandName"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Type</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Type"
-                  className="input input-bordered"
-                  name="type"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Price</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Price"
-                  className="input input-bordered"
-                  name="price"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Short description</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Short description"
-                  className="input input-bordered"
-                  name="shortDescription"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Rating</span>
-                </label>
-                <input
-                  type="type"
-                  placeholder="Rating"
-                  className="input input-bordered"
-                  name="rating"
+                  name="imageURL"
                   required
                 />
               </div>
             </div>
             <div className="form-control mt-6">
               <button className="btn bg-opacity-0 border-[#59CE8F] hover:bg-opacity-0 hover:border-orange-200 w-[20%] mx-auto">
-                Add
+                Post
               </button>
             </div>
           </form>
@@ -138,7 +172,8 @@ const AddProduct = () => {
         </div>
       </div>
     </div>
+    </div>
   );
 };
 
-export default AddProduct;
+export default AddJob;
