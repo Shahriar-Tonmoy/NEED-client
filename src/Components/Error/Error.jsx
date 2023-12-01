ee34d5ab644af66de6f51a3e735dce7c1f8aa829
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-01 07:11:47 +0600

    Error page added

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -5,6 +5,7 @@ const Error = () => {
             {/* <h1 className="text-3xl md:text-8xl text-red-600">404 Error!</h1> */}
            
             <Link to='/'><button className="text-green-600 text-3xl border border-green-700 absolute left-auto">&lt;Go to Home</button></Link>
+            
         </div>
     );
 };
